33d73ba2eef22afc8a0ff36818d1ecacd10ab26c
DuanyiSu <duanyi.su@mail.utoronto.ca>
2024-09-27 03:56:00 -0400

    Update 07-replications.R

--- scripts/07-replications.R
+++ scripts/07-replications.R
@@ -1,19 +1,22 @@
 #### Preamble ####
-# Purpose: Replicated graphs from... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Replicates graphs to visualize red light camera data trends and patterns
+# Author: Duanyi Su
+# Date: 27 September 2024
+# Contact: duanyi.su@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: Requires the 'tidyverse' package for data visualization
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Load data ####
-# [...ADD CODE HERE...]
+analysis_data <- read_csv("data/analysis_data/cleaned_red_light_data.csv")
 
-
-
+#### Replicate graphs ####
+# Example: Plotting the trend of violations over the years
+ggplot(analysis_data, aes(x = year_installed, y = violations)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  labs(title = "Red Light Violations Over Time",
+       x = "Year Installed",
+       y = "Number of Violations")
