a01ecdc631c9eb8bb8a576982a2ee960790df2bc
DuanyiSu <duanyi.su@mail.utoronto.ca>
2024-09-27 03:49:54 -0400

    Update 02-download_data.R

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,26 +1,21 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the red light camera data from Open Data Toronto
+# Author: Duanyi Su
+# Date: 27 September 2024
+# Contact: duanyi.su@example.com
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: Install the opendatatoronto and tidyverse packages.
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
-
+# Downloading the red light camera data
+rlc_package <- show_package("red-light-cameras")
+rlc_resource <- list_package_resources(rlc_package) %>% filter(name == "Red Light Camera Locations")
+the_raw_data <- rlc_resource %>% get_resource()
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+# Save the downloaded data
+write_csv(the_raw_data, "data/raw_data/unedited_data.csv")
